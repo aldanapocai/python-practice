ced3e517da23f16cc45320473d7f1e877f7f588b
aldanapocai <aldanapocai@gmail.com>
2020-11-02 23:15:21 -0300

    Modificacion y entrega de mareas_a_mano.py

--- Ejercicios/mareas_a_mano.py
+++ Ejercicios/mareas_a_mano.py
@@ -9,7 +9,7 @@ mareas_fft.py
 
 import pandas as pd
 
-df = pd.read_csv('Data/OBS_SHN_SF-BA.csv', index_col = 'Time', parse_dates = True)
+df = pd.read_csv('Data/OBS_SHN_SF-BA.csv', index_col = ['Time'], parse_dates = True)
 
 
 dh = df['12-25-2014': ].copy()
